88dbe777aef462062e5bb1bfc8328ffbd206052a
Mango <thoconbatron1@gmail.com>
2022-01-23 21:28:36 -0800

    Added a simple web server

--- main.tf
+++ main.tf
@@ -5,6 +5,11 @@ provider "aws" {
 resource "aws_instance" "awesome" {
   instance_type = "t2.micro"
   ami = "ami-083602cee93914c0c"
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
   tags = {
     Name = "Awesome-VM"
   }
